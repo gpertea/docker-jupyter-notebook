fddefd1b8854d1d7183b35d972c9b2780bac1aa8
Geo Pertea <geo.pertea@gmail.com>
2019-04-23 13:54:17 -0400

    simply changed the minimum number of active connections from 3 to 2

--- monitor_traffic.sh
+++ monitor_traffic.sh
@@ -5,13 +5,13 @@
 # Indicates that the server has received the first FIN signal from the client
 # and the connection is in the process of being closed. But that can never happen.
 # For some reason there are a few connections open that do not relate the the
-# client that needs to be connected over the port :6789. If we do not have a
-# connection open from port 6789, kill the server and herewith the docker container.
+# client that needs to be connected over the port :8888. If we do not have a
+# connection open from port 8888, kill the server and herewith the docker container.
 
 while true; do
     sleep ${DEFAULT_CONTAINER_RUNTIME:-240}
 
-    if [ `netstat -t | grep -v CLOSE_WAIT | grep ':8888' | wc -l` -lt 3 ]
+    if [ `netstat -t | grep -v CLOSE_WAIT | grep ':8888' | wc -l` -lt 2 ]
     then
         pkill jupyter
     fi
